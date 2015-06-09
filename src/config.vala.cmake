f6b5ed11e262e37e5006c35f400ef434c8935593
Damien Plenard <damien@plenard.me>
2015-06-10 01:03:28 +0200

    add cmake file to compile application

---
+++ src/config.vala.cmake
@@ -0,0 +1,10 @@
+namespace Build {
+  public const string DATADIR = "@DATADIR@";
+  public const string PKGDATADIR = "@PKGDATADIR@";
+  public const string ICON_DIR = "@ICON_DIR@";
+  public const string GETTEXT_PACKAGE = "@GETTEXT_PACKAGE@";
+  public const string RELEASE_NAME = "@RELEASE_NAME@";
+  public const string VERSION = "@VERSION@";
+  public const string VERSION_INFO = "@VERSION_INFO@";
+  public const string CMAKE_INSTALL_PREFIX = "@CMAKE_INSTALL_PREFIX@";
+}
